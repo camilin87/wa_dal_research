4ef21c6f4bdd6e31e861e7f1ae4d20ad008b569e
Camilo Sanchez <camilin87@gmail.com>
2014-03-22 16:11:15 -0400

    set a redis valud

--- test_redis.py
+++ test_redis.py
@@ -1,4 +1,16 @@
+# development
+# > brew install redis
+# > sudo pip install redis
+# > redis-server
+
 # deployment
 # > sudo apt-get install redis-server
 # > sudo pip install redis
 
+import redis
+
+r = redis.StrictRedis(host="localhost", port=6379, db=0)
+
+r.set("foo", "bar")
+
+print r.get("foo")
